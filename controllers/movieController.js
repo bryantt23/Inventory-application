f487b376ff8df7a1ccd7f2d2daeb5086f4c6bca2
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-01-10 20:17:42 -0800

    Change style to click on entire div of movie in list

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -5,7 +5,7 @@ var async = require('async');
 
 const { movie, validationResult } = require('express-validator');
 
-// Display list of all Books.
+// Display list of all Movies.
 exports.movie_list = function (req, res, next) {
   Movie.find({}, 'title summary year imageUrl')
     .populate('genre')
@@ -19,4 +19,32 @@ exports.movie_list = function (req, res, next) {
         movie_list: list_movies
       });
     });
+};
+
+// Display detail page for a specific movie.
+exports.movie_detail = function (req, res, next) {
+  async.parallel(
+    {
+      movie: function (callback) {
+        Movie.findById(req.params.id).populate('genre').exec(callback);
+      }
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.movie == null) {
+        // No results.
+        var err = new Error('Movie not found');
+        err.status = 404;
+        return next(err);
+      }
+      // Successful, so render.
+      res.render('movie_detail', {
+        title: results.movie.title,
+        movie: results.movie,
+        movie_instances: results.movie_instance
+      });
+    }
+  );
 };
--- routes/catalog.js
+++ routes/catalog.js
@@ -5,7 +5,10 @@ var router = express.Router();
 var movie_controller = require('../controllers/movieController');
 // var genre_controller = require('../controllers/genreController');
 
-// GET request for list of all Book items.
+// GET request for list of all Movie items.
 router.get('/movies', movie_controller.movie_list);
+
+// GET request for one Movie.
+router.get('/movie/:id', movie_controller.movie_detail);
 
 module.exports = router;
